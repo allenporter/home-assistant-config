c9276f5c558a93e5ba1c2cfa9e83aa5c86249982
Allen Porter <allen.porter@gmail.com>
2024-08-25 15:46:04 +0000

    Update notify agenda tests to pass after demo platform updates

--- tests/test_notify_agenda.py
+++ tests/test_notify_agenda.py
@@ -11,7 +11,7 @@ import pytest
 from freezegun import freeze_time
 import yaml
 
-from homeassistant.core import HomeAssistant, Event
+from homeassistant.core import HomeAssistant, Event, ServiceCall
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.setup import async_setup_component
 from homeassistant.const import Platform
@@ -20,6 +20,7 @@ from pytest_homeassistant_custom_component.common import (
     MockConfigEntry,
     async_fire_time_changed,
     async_capture_events,
+    async_mock_service,
 )
 
 
@@ -27,7 +28,8 @@ _LOGGER = logging.getLogger(__name__)
 
 
 AUTOMATION_YAML = pathlib.Path("config/automations/notify_agenda.yaml")
-
+NOTIFY_ENTITY = "notify.notifier"
+WEATHER_ENTITY = "weather.demo_weather_north"
 
 @pytest.fixture(autouse=True)
 async def mock_default_components(hass: HomeAssistant) -> None:
@@ -57,12 +59,28 @@ async def mock_weather_demo(hass: HomeAssistant) -> MockConfigEntry:
     ):
         await hass.config_entries.async_setup(config_entry.entry_id)
     assert config_entry.state == ConfigEntryState.LOADED
+
+    state = hass.states.get(WEATHER_ENTITY)
+    assert state is not None
+
     return config_entry
 
 
 @pytest.fixture(name="notify")
 async def mock_notify(hass: HomeAssistant) -> MockConfigEntry:
-    assert await async_setup_component(hass, "notify", {"notify": {"platform": "demo"}})
+    config_entry = MockConfigEntry(domain="demo")
+    config_entry.add_to_hass(hass)
+    with patch(
+        "homeassistant.components.demo.COMPONENTS_WITH_CONFIG_ENTRY_DEMO_PLATFORM",
+        [Platform.NOTIFY],
+    ):
+        await hass.config_entries.async_setup(config_entry.entry_id)
+    assert config_entry.state == ConfigEntryState.LOADED
+
+    state = hass.states.get(NOTIFY_ENTITY)
+    assert state is not None
+
+    return config_entry
 
 
 @pytest.fixture(name="template")
@@ -74,12 +92,14 @@ async def mock_template(
 ) -> None:
     with AUTOMATION_YAML.open("r") as fd:
         content = fd.read()
-        content = content.replace("weather.woodgreen", "weather.demo_weather_north")
+        content = content.replace("weather.woodgreen", WEATHER_ENTITY)
         content = content.replace(
             "conversation_agent: 2ee2edd1e9dbee5de7474922ce3cee42",
             "conversation_agent: homeassistant",
         )
-        content = content.replace("notify.discord", "notify.notify")
+        content = content.replace("notify_service: notify.discord", "notify_service: notify.persistent_notification")
+        content = content.replace("notify_target: notify.discord", "notify_target: notify.notifier")
+        print(content)
         config = yaml.load(content, Loader=yaml.Loader)
 
     assert await async_setup_component(hass, "automation", {"automation": config})
@@ -88,9 +108,9 @@ async def mock_template(
 
 
 @pytest.fixture
-def events(hass: HomeAssistant) -> list[Event[Mapping[str, Any]]]:
+def notify_service_calls(hass: HomeAssistant) -> list[ServiceCall]:
     """Fixture that catches notify events."""
-    return async_capture_events(hass, "notify")
+    return async_mock_service(hass, "notify", "persistent_notification")
 
 
 @pytest.mark.parametrize(("expected_lingering_timers"), [True])
@@ -98,7 +118,7 @@ async def test_notify_agenda(
     hass: HomeAssistant,
     template: Any,
     error_caplog: pytest.LogCaptureFixture,
-    events: list[Event[Mapping[str, Any]]],
+    notify_service_calls: list[ServiceCall],
 ) -> None:
     """Collects model responses for area summaries."""
 
@@ -112,12 +132,12 @@ async def test_notify_agenda(
         async_fire_time_changed(hass, next)
         await hass.async_block_till_done()
 
-    assert len(events) == 1
-    event = events[0].as_dict()["data"]
-    assert "Agenda" in event.get("title")
+    assert len(notify_service_calls) == 1
+    data = notify_service_calls[0].data
+    assert "Agenda" in data.get("title")
 
     # We're using the default agent for testing
-    assert "couldn't understand that" in event.get("message")
+    assert "couldn't understand that" in data.get("message")
 
     # Automation completes with success
     assert not error_caplog.records
